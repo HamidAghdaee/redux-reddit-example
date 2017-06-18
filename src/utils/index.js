bd52f5084355dcf7d555f0f99c7beee8c2c5b3be
Hamid Aghdaee <hamid.aghdaee@galvanize.com>
2017-06-18 10:25:15 -0700

    removed utils

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-import pathToRegexp from 'path-to-regexp'
-
-export const compilePath = function compilePath(pattern, options) {
-  const keys = []
-  const re = pathToRegexp(pattern, keys, options);
-  const compiledPattern = { re: re, keys: keys };
-
-  return compiledPattern;
-};
